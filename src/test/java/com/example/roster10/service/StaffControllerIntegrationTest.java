a70fbe794fa553576a07e828e5748defc43af6f3
hiro903 <hirokonosub@gmail.com>
2024-06-07 00:26:57 +0900

    ExpectedDataSetを活用したコードの書き直し

--- src/test/java/com/example/roster10/service/StaffControllerIntegrationTest.java
+++ src/test/java/com/example/roster10/service/StaffControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.example.roster10.service;
 import com.example.roster10.Staff;
 import com.example.roster10.StaffRequest;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
 
@@ -31,30 +33,26 @@ public class StaffControllerIntegrationTest {
     }
 
     @Test
+    @Sql("/sqlannotation/delete-staff.sql")
     @DataSet("datasets/staff.yml")
+    @ExpectedDataSet("expected_datasets/after_insert.yml")
     public void スタッフの情報を渡したら登録されてスタッフの情報が返ってくること() {
-        StaffRequest staffRequest = new StaffRequest("anna", LocalDate.of(1998, 1, 31), "Tokyo");
+        StaffRequest staffRequest = new StaffRequest("Anna", LocalDate.of(2001, 1, 1), "Tokyo");
         ResponseEntity<Staff> postResponse = restTemplate.postForEntity(getRootUrl() + "/staff", staffRequest, Staff.class);
         assertThat(postResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
-        assertThat(postResponse).isNotNull();
         Staff result = postResponse.getBody();
         assertThat(result).isNotNull();
-        assertThat(result.getName()).isEqualTo(staffRequest.getName());
-        assertThat(result.getDateOfBirth()).isEqualTo(staffRequest.getDateOfBirth());
-        assertThat(result.getNearestStation()).isEqualTo(staffRequest.getNearestStation());
-
-        ResponseEntity<Staff> getResponse = restTemplate.getForEntity(getRootUrl() + "/staff/" + result.getId(), Staff.class);
-        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Staff getResult = getResponse.getBody();
-        assertThat(getResult).isNotNull();
-        assertThat(getResult.getName()).isEqualTo("anna");
+        if (result != null) {
+            assertThat(result.getName()).isEqualTo("Anna");
+        }
     }
 
     @Test
     @DataSet("datasets/staff.yml")
+    @ExpectedDataSet("expected_datasets/after_update.yml")
     public void 指定したIDのスタッフの情報を渡したら更新されること() {
-        Staff updatedInfo = new Staff(1, "Anna", LocalDate.of(2001, 1, 1), "Shibuya");
+        Staff updatedInfo = new Staff(1, "Anna", LocalDate.of(2001, 1, 1), "Tokyo");
 
         restTemplate.put(getRootUrl() + "/staff/1", updatedInfo);
 
@@ -65,15 +63,17 @@ public class StaffControllerIntegrationTest {
 
     @Test
     @DataSet("datasets/staff.yml")
+    @ExpectedDataSet("expected_datasets/after_find_all.yml")
     public void 登録しているスタッフの情報を全件取得すること() {
         ResponseEntity<Staff[]> response = restTemplate.getForEntity(getRootUrl() + "/staff", Staff[].class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody()).hasSize(3);
+        assertThat(response.getBody()).hasSize(3); // Assuming 3 staff members after setup
     }
 
     @Test
     @DataSet("datasets/staff.yml")
+    @ExpectedDataSet("expected_datasets/after_delete.yml")
     public void 登録しているスタッフの情報のIDを指定して削除すること() {
         restTemplate.delete(getRootUrl() + "/staff/1");
         ResponseEntity<Staff> getResponse = restTemplate.getForEntity(getRootUrl() + "/staff/1", Staff.class);
@@ -82,6 +82,7 @@ public class StaffControllerIntegrationTest {
 
     @Test
     @DataSet("datasets/staff.yml")
+    @ExpectedDataSet("expected_datasets/staff_not_found.yml")
     public void 登録されていないIDを指定したらエラーコードが返ってくること() {
         ResponseEntity<Staff> response = restTemplate.getForEntity(getRootUrl() + "/staff/999", Staff.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
@@ -89,6 +90,7 @@ public class StaffControllerIntegrationTest {
 
     @Test
     @DataSet("datasets/staff.yml")
+    @ExpectedDataSet("expected_datasets/after_delete_not_found.yml")
     public void 登録されていないIDを指定して削除しようとしたらエラーコードが返ってくること() {
         restTemplate.delete(getRootUrl() + "/staff/999");
         ResponseEntity<Staff> getResponse = restTemplate.getForEntity(getRootUrl() + "/staff/999", Staff.class);
@@ -97,6 +99,7 @@ public class StaffControllerIntegrationTest {
 
     @Test
     @DataSet("datasets/staff.yml")
+    @ExpectedDataSet("expected_datasets/update_not_found.yml")
     public void 登録されていないIDを指定して更新しようとしたらエラーコードが返ってくること() {
         Staff updatedInfo = new Staff(999, "Anna", LocalDate.of(2001, 1, 1), "Shibuya");
 
