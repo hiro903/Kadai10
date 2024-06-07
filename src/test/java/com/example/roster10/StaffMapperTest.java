c4ee14d178c01c70e47e125e9a758fb9d4204bab
hiro903 <hirokonosub@gmail.com>
2024-06-07 23:25:59 +0900

    StaffMapperIntegrationTestの書き直しとStaffRestApiIntegrationTestの直し

--- src/test/java/com/example/roster10/StaffMapperTest.java
+++ src/test/java/com/example/roster10/StaffMapperTest.java
@@ -1,61 +1,50 @@
 package com.example.roster10;
 
-import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @DBRider
-@SpringBootTest(classes = StaffApplication.class)
+//@SpringBootTest(classes = StaffApplication.class)
+@MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@AutoConfigureMockMvc
-class StaffMapperTest {
+class StaffMapperIntegrationTest {
+
     @Autowired
-    MockMvc mockMvc;
+    private StaffMapper staffMapper;
 
     @Test
-    @DataSet(value = "datasets/staff.yml")
+    @Sql(
+            scripts = {"classpath:/sqlannotation/delete-staff.sql", "classpath:/sqlannotation/insert-staff.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
     @Transactional
-    void ユーザーが取得できること() throws Exception {
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/staff"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+    void ユーザーが取得できること() {
+        List<Staff> staffList = staffMapper.findAll();
 
-        String expectedResponse = """
-                [
-                    {
-                        "id": 1,
-                        "name": "chika",
-                        "dateOfBirth": "2000-07-01",
-                        "nearestStation": "Tokyo"
-                    },
-                    {
-                        "id": 2,
-                        "name": "airi",
-                        "dateOfBirth": "2005-08-13",
-                        "nearestStation": "Meguro"
-                    },
-                    {
-                        "id": 3,
-                        "name": "nanami",
-                        "dateOfBirth": "1998-10-25",
-                        "nearestStation": "Kichijoji"
-                    }
-                ]
-                """;
+        List<Staff> expectedList = List.of(
+                new Staff(1, "chika", LocalDate.of(2000, 7, 1), "Tokyo"),
+                new Staff(2, "airi", LocalDate.of(2005, 8, 13), "Meguro"),
+                new Staff(3, "nanami", LocalDate.of(1998, 10, 25), "Kichijoji")
+        );
 
-        JSONAssert.assertEquals(expectedResponse, response, JSONCompareMode.STRICT);
+        assertEquals(expectedList.size(), staffList.size());
+
+        for (int i = 0; i < expectedList.size(); i++) {
+            assertEquals(expectedList.get(i).getId(), staffList.get(i).getId());
+            assertEquals(expectedList.get(i).getName(), staffList.get(i).getName());
+            assertEquals(expectedList.get(i).getDateOfBirth(), staffList.get(i).getDateOfBirth());
+            assertEquals(expectedList.get(i).getNearestStation(), staffList.get(i).getNearestStation());
+        }
     }
 }
